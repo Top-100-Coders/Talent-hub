1471cfc9f203e24a8c140003d31fff1b5b5c037d
Aanand Deva Suresh <122597947+Keyes1@users.noreply.github.com>
2023-11-20 00:00:52 +0530

    Add files via upload

---
+++ db_query_api/db_query_api.py
@@ -0,0 +1,85 @@
+from flask import Flask, request, make_response, request
+from flask_cors import CORS
+import json
+import pymongo
+from bson import ObjectId
+import os
+
+
+class CustomJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, ObjectId):
+            return str(obj)
+        return super().default(obj)
+
+
+app = Flask(__name__)
+CORS(app)
+
+host = 'localhost'
+port = 27017
+username = os.environ.get("DB_USERNAME")
+password = os.environ.get("DB_PASSWORD")
+
+connection_string = f'mongodb://{username}:{password}@{host}:{port}'
+client = pymongo.MongoClient(connection_string)
+
+
+# Choose the database and collection
+db = client['mulearn']
+collection = db['student_profiles']
+
+
+def search_candidates(name=None, location=None, karma=None):
+    # Build the query based on the given criteria
+    query = {}
+
+    if name:
+        query['Name'] = {'$regex': name, '$options': 'i'}
+
+    if location:
+        query['Location'] = location
+
+    if karma is not None:
+        query['Karma'] = {'$gte': karma}
+
+    # Execute the query and fetch the results
+    query_results = list(collection.find(query))
+
+    r = make_response(json.dumps(query_results, cls=CustomJSONEncoder))
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+@app.route('/search', methods=['GET'])
+def search():
+    name = request.args.get('name')
+    location = request.args.get('location')
+    karma = request.args.get('karma')
+    if karma is not None:
+        karma = int(karma)
+
+    candidates = search_candidates(name, location, karma)
+    return candidates
+
+
+@app.route('/add_data', methods=['POST'])
+def add_data():
+    data = request.get_json()
+    values = data.get('values')
+
+    if not isinstance(values, list):
+        r = make_response(json.dumps(
+            {'error': 'Invalid request body format'}, cls=CustomJSONEncoder))
+        r.headers['Content-Type'] = 'application/json'
+        return r
+
+    result = collection.insert_many(values)
+    r = make_response(json.dumps(
+        {'ids': result}, cls=CustomJSONEncoder))
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
